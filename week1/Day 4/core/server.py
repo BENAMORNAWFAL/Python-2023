3fb55f27939b232186b67871df6eb054be6653cd
BENAMORNAWFAL <technobenamor@gmail.com>
2023-05-22 08:46:49 +0100

    Playground flask

---
+++ week1/Day 4/core/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app= Flask(__name__)
+
+@app.route('/play')
+def play():
+   return render_template('index.html')
+
+@app.route('/play/<number>')
+def playtimes(number):
+   return render_template('index.html',times=int(number))
+
+@app.route('/play/<number>/<color>')
+def playcolor(number,color):
+   return render_template('index.html',times=int(number),txt_color=color)
+
+
+
+
+
+if __name__=='__main__' :
+    app.run(debug=True)
