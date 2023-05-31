8825ecc414151c51c760fb8e4ba79bc99a59fd81
BENAMORNAWFAL <technobenamor@gmail.com>
2023-05-31 15:10:42 +0100

    dojo_and_ninjas_platform

---
+++ week2/Day4/core/ninjas_and_dojos/flask_app/controllers/dojos.py
@@ -0,0 +1,29 @@
+from flask_app import app
+from flask import render_template, session,request, redirect
+from flask_app.models.dojo import Dojo
+from flask_app.models.ninja import Ninja
+
+#=========================rout display======================
+@app.route("/")
+def root():
+    return redirect('/dojo')
+
+#=======================insert dojos & display dojos ===================
+@app.route("/create_dojo",methods=["POST"])
+def add_dojo():
+    
+    Dojo.create_dojo(request.form)
+    return redirect("/dojo")
+
+@app.route("/dojo")
+def home_page():
+    all_dojo=Dojo.show_dojos()
+    return render_template("dojospage.html",all_dojo=all_dojo)
+
+@app.route('/dojo/<int:dojo_id>')
+def show_dojo(dojo_id):
+    data={
+        'id':dojo_id
+    }
+    show=Ninja.show_ninja_in_dojo(data)
+    return render_template('show_dojo.html',show=show)
---
+++ week2/Day4/core/ninjas_and_dojos/flask_app/controllers/ninjas.py
@@ -0,0 +1,23 @@
+from flask_app import app
+from flask import render_template, session,request, redirect
+from flask_app.models.dojo import Dojo
+from flask_app.models.ninja import Ninja
+
+#============================================================= read ninjas==================================
+@app.route('/ninjas',)
+def ninja():
+    all_dojos=Dojo.show_dojos()
+    return render_template("addNinja.html",all_dojos=all_dojos)
+
+# ============================================================create ninjas===================================
+@app.route("/add_ninjas",methods=['POST'])
+def add_new_ninja():
+
+    data={
+        'first_name':request.form['first_name'],
+        'last_name':request.form['last_name'],
+        'age':request.form['age'],
+        'dojo_id':request.form['dojo_id']
+    }
+    Ninja.create_ninja(data)
+    return redirect('/ninjas')
